00e28efe1898e44494227c73c37a3f0cf98cbcf0
ParasKumarGupta <55997716+ParasKumarGupta@users.noreply.github.com>
2021-07-14 22:50:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nimble/Activity/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.nimble.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.nimble.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2500);
+    }
+}
